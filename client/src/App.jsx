e61d8abf562103b604d8261b85921e7ce9d4bc8e
Marinela Togoe <marinelatogoe@gmail.com>
2023-08-11 16:49:32 +0200

    add basic router for home and about pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { RouterProvider,  createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>about page</h1>
+      </div>
+    ),
+  },
+]);
 
 const App = () => {
-  return (
-    <h1>
-      devyApp
-    </h1>
-  )
+  return <RouterProvider router={router} />;
+ 
 }
 
-export default App
+export default App;
