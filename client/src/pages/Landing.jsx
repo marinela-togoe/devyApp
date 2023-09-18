9160d523044888b23758164a84d602aaca147272
Marinela Togoe <marinelatogoe@gmail.com>
2023-09-18 21:11:41 +0200

    add a template of a wrapper component inside the landing page for styling purposes

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const StyledBtn = styled.button`
-font-size:1.5rem;
-background: red;
-color: white;
-`;
 
 const Landing = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>Landing</h1>
-      <StyledBtn>styled btn</StyledBtn>
-    </div>
+      <div className="content">some content</div>
+    </Wrapper>
   );
+};
+
+const Wrapper = styled.div`
+background: red;
+h1 {
+  color: white;
 }
+.content {
+  background: blue;
+  color: yellow;
+}
+`;
+
 
 export default Landing;
