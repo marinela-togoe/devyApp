6c46048dccd832666e0b53b968b476926bcfdb59
Marinela Togoe <marinelatogoe@gmail.com>
2023-10-02 18:55:33 +0200

    Add the Logo component into the components folder

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/images/logo.svg";
+
+
+const Logo = () => {
+    return (
+        <img src={logo} alt="devyApp" className="logo" />
+      
+    )
+};
+
+
+export default Logo;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Wrapper from '../assets/wrappers/LandingPage';
 import main from '../assets/images/main.svg';
-import logo from '../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="devyApp" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
