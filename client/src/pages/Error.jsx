d76c486a7070e097bb8ef1615e3dc973af96e205
Marinela Togoe <marinelatogoe@gmail.com>
2023-10-02 19:26:30 +0200

    add if statement functionality to the error page to display a better UX image.

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
+
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+  
+
+  if(error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ooops! Page not found.</h3>
+          <p>The page you are looking for does not existing.</p>
+          <Link to='/dashboard'>Main Page</Link>
+        </div>
+      </Wrapper>
+    )
+  }
   return (
-    <div>
-      <h1>Error</h1>
-      <Link to="/">back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong...</h3>
+      </div>
+    </Wrapper>
   );
 }
 
